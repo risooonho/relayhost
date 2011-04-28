a8c9140847ec1aa0cab0cc757f90654c694de0d0
Andrea Piras <braindamage@springlobby.info>
2011-04-28 23:14:13 +0200

    fixed typo

--- help.py
+++ help.py
@@ -5,8 +5,9 @@ from tasbot.Plugin import IPlugin
 class Main(IPlugin):
 	def __init__(self,name,tasclient):
 		IPlugin.__init__(self,name,tasclient)
-		
+
 	def oncommandfromserver(self,command,args,socket):
 		if command == "SAIDPRIVATE" and len(args) > 1 and args[1] == "!help":
 			for l in helptext.split("\n"):
-				self.tasclient..send("SAYPRIVATE %s %s\n" % (args[0],l))
+				self.tasclient.send("SAYPRIVATE %s %s\n" % (args[0],l))
+
